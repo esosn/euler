38894c0023b429d97555121a35e54387e17fbf7f
Thresh <esotericscreenname@gmail.com>
2015-03-14 11:08:11 +0800

    Solution to problem 22

---
+++ 22.py
@@ -0,0 +1,21 @@
+# O(n log n) [timsort] + O(n) [summation], naive
+# complexity reduces to O(n log n)
+
+def score(name):
+    value = 0
+    for l in name:
+        if(l.isalpha()):
+            value += ord(l) - 64
+    return value
+
+total = 0
+fo = open('euler/p022_names.txt')
+raw = fo.read()
+fo.close()
+
+names = raw.split(',')
+names.sort()
+for i in range(len(names)):
+    total += score(names[i]) * (i+1)
+print(total)
+
