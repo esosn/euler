20249518a4808012237d2a4a82f57aa2789babc3
Thresh <esotericscreenname@gmail.com>
2015-03-21 14:41:33 +0800

    Optimizations

--- 49.py
+++ 49.py
@@ -17,11 +17,17 @@ def primes1(n):
     return [2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
 primes = primes1(10000)[168:] #first 4 digit prime is #169
 pset = set(primes)
+pset.discard(1487) # known answer
 
+times.append(time.clock())
+print(times[-1]-times[-2])
+
+# naive search
+# generates permutations instead of testing them
 def permute(p):
     global pset
     pset.discard(p)
-    if not p + 3330 in pset: return
+    if not (p + 3330 in pset and p + 6660 in pset): return
     perms = [p]
     s = str(p)
     q = deque(s)
@@ -53,11 +59,33 @@ def permute(p):
                 values = values + [perms[i],perms[j]]
     if len(values) >= 3:
         print(str(values[0])+str(values[1])+str(values[3]))
+        return True
+    return False
 
 #generate sets
 for p in primes:
     if not p in pset: continue
-    permute(p)
+    if permute(p): break
+    
+times.append(time.clock())
+print(times[-1]-times[-2])
+
+# more intelligent search, ~4x faster
+pset = set(primes)
+
+def ispermutation(q,r):
+    x = sorted(str(q))
+    y = sorted(str(r))
+    return all([a==b for (a,b) in zip(x, y)])
+    
+for p in primes[:-2]:
+    if p == 1487: continue
+    p1 = p + 3330
+    p2 = p + 6660
+    if not (p1 in pset and p2 in pset): continue
+    if ispermutation(p,p1) and ispermutation(p,p2):
+        print(str(p)+str(p1)+str(p2))
+        break
 
 times.append(time.clock())
-print(times[len(times)-1]-times[len(times)-2])
+print(times[-1]-times[-2])
