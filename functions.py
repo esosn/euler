52f522aa99b50f9758c14ac6b99db3d5b923fd66
Thresh <esotericscreenname@gmail.com>
2015-03-30 23:53:00 +0800

    Improving sundaram function

--- functions.py
+++ functions.py
@@ -85,7 +85,6 @@ def permute(s, dq, depth, maxdepth, callback, cb_args):
 def sundaram(n):
     """ Returns  a list of primes < n """
     sieve = [True] * (n // 2)
-    primes = set([1] * (n // 2))
     for i in range(3, int(n**0.5) + 1, 2):
         if sieve[i // 2]:
             sieve[i*i // 2::i] = [False] * ((n - i*i - 1) // (2*i) + 1)
