866e05404387c29b500e344eab72b753c152987b
esosn <esotericscreenname@gmail.com>
2015-03-31 20:55:04 +0800

    New solution

---
+++ 48.py
@@ -0,0 +1,14 @@
+import time
+import math
+from array import array
+times = []
+times.append(time.clock())
+limit = 1000
+
+# brute force is really easy on this one
+# done more naturally with str slicing, but mod is a little faster here
+
+print((10405071317 + sum(i**i for i in range(11, limit))) % 10**10)
+
+times.append(time.clock())
+print(times[-1] - times[-2])
