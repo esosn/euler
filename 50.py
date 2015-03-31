8fc73332449c606c5aabea44226d9d5af7e1d4fb
Thresh <esotericscreenname@gmail.com>
2015-04-01 00:17:13 +0800

    New solution

---
+++ 50.py
@@ -0,0 +1,37 @@
+import time
+import math
+from functions import sundaram
+times = []
+times.append(time.clock())
+
+limit = 1000000
+primes = sundaram(limit)
+pset = set(primes)
+
+# search with a sliding window that shrinks in size
+
+maxi = len(primes)
+start = sum(primes[:-1])
+stop = False
+if start in pset:
+    print(start)
+else:  
+    for i in range(maxi - 2, 2, -1):
+        x = start
+        for j in range(maxi - i):
+            x = x - primes[j] + primes[j + i + 1]
+            if x > limit:
+                break
+            if x in pset:
+                print(x)
+                stop = True
+                break
+        if stop:
+            break
+        start -= primes[i]
+        if start in pset:
+            print(start)
+            break
+        
+times.append(time.clock())
+print(times[-1] - times[-2])
